37d86b27ca1c86c7df9475aff99551e82d6e0edb
= <hlmmit001@myuct.ac.za>
2019-02-20 08:49:06 +0200

    Finished CocoFramework

---
+++ CocoFramework/CocoFramework/CocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  CocoFramework.h
+//  CocoFramework
+//
+//  Created by Mitchell Holmes on 2019/02/20.
+//  Copyright © 2019 Mitchell Holmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoFramework.
+FOUNDATION_EXPORT double CocoFrameworkVersionNumber;
+
+//! Project version string for CocoFramework.
+FOUNDATION_EXPORT const unsigned char CocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoFramework/PublicHeader.h>
+
+
---
+++ CocoFramework/CocoFramework/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  CocoFramework
+//
+//  Created by Mitchell Holmes on 2019/02/20.
+//  Copyright © 2019 Mitchell Holmes. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {
+        
+    }
+    
+    public static func DoSomething() -> String {
+        return "Did some stuff yo!"
+    }
+}
